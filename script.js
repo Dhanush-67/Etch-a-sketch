7adec049a626744b70b20cd9c8cc1d96a9aa823f
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-08-06 22:59:23 -0400

    Created the drawing grid using flexbox and js

---
+++ script.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    div.style.width = "25px";
+    div.style.height = "25px";
+    div.style.border = "1px solid black";
+    const container = document.querySelector(".container");
+    container.appendChild(div);
+  }
+}
